bf2ec84aa4c05991c1f2bb9c8fbd573a0d4125e5
user <user@users-MacBook-Air.local>
2023-06-21 21:33:22 +0700

    change media query of Obsviewsly  on conatiner add xl:px-60

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -2,8 +2,10 @@ import React from 'react'
 import './index.css'
 export const Loader = () => {
     return (
-        <div className="box items-center justify-center self-center">
-            <div className="plane"></div>
+        <div className='items-center justify-center self-center'>
+            <div className="box ">
+                <div className="plane"></div>
+            </div>
         </div>
     )
 }
