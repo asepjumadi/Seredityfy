8e4001160983a04727077dcbd4a2f605a520d15c
user <user@users-MacBook-Air.local>
2023-06-23 16:40:31 +0700

    add updatesModels.js | add Button1,..etc to imports|implement reusable component though update roadmap

--- src/components/Update/UpdateCard.jsx
+++ src/components/Update/UpdateCard.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 
-const UpdateCard = () => {
-  return (
-    <div className='max-w-xs items-center bg-blue-950 border border-slate-200 bg-opacity-30  rounded-lg shadow  dark:border-gray-700 justify-center  backdrop-blur-3xl '>
-        <div className='flex flex-row '>
-            <img/>
+const UpdateCard = ({ icons,title ,heading,headingContent,firstListText,secondListText,isDetail}) => {
+    return (
+        <div className='max-w-xs items-center bg-blue-950 border border-slate-200 bg-opacity-30  rounded-lg shadow  dark:border-gray-700 justify-center  backdrop-blur-3xl '>
+            <div className='flex flex-row '>
+                <div className='flex flex-col'>
+                    <img src={icons} className='w-20 h-20' alt="safe partice button" />
+                    <div className='w-1 h-full bg-slate-300'/>
+                </div>
+                <div>
+                    <h1 className='text-white text-2xl leading-4 font-bold'>{title}</h1>
+                    <h3 className='text-blue-300 text-xl '>{heading}<span className='text-slate-400 text-xl  '>{headingContent}</span></h3>
+                   <ul>
+                    <li className='text-gray-400 text-sm'>{firstListText}</li>
+                    <li className='text-gray-400 text-sm'>{secondListText}</li>
+                   </ul>
+                   {
+                    isDetail&&
+                    <><h1>follow up</h1></>
+                   }
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default UpdateCard
---
+++ src/model/UpdateModels.js
@@ -0,0 +1,30 @@
+import { Button1,Button2,Button3 } from "../assets/import";
+export const dataSetUpdates=[
+    {
+        icons:Button1,
+        title:"Feature",
+        heading:"/DESCRIBE",
+        headingContent:"RELEASE ,APRIL 21 ,2023",
+        firstListText:"We have added an NSFW image filter detector that works similarly to the image prompt filter.",
+        secondListText:"We have added an NSFW image filter detector that works similarly to the image prompt filter.",
+        isDetail:false,
+    },
+    {
+        icons:Button2,
+        title:"Feature",
+        heading:"/DESCRIBE",
+        headingContent:"RELEASE ,APRIL 21 ,2023",
+        firstListText:"We have added an NSFW image filter detector that works similarly to the image prompt filter.",
+        secondListText:"We have added an NSFW image filter detector that works similarly to the image prompt filter.",
+        isDetail:false,
+    },
+    {
+        icons:Button3,
+        title:"Feature",
+        heading:"/DESCRIBE",
+        headingContent:"RELEASE ,APRIL 21 ,2023",
+        firstListText:"We have added an NSFW image filter detector that works similarly to the image prompt filter.",
+        secondListText:"We have added an NSFW image filter detector that works similarly to the image prompt filter.",
+        isDetail:false,
+    }
+]
