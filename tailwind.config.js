e604a90db91a8d4420be2cafb15f388b3047c322
Asep Jumadi <43208840+asepjumadi@users.noreply.github.com>
2023-06-23 23:10:43 +0700

    update tailwinds.config.js
    add  tailwinds read content such as jsx nad tsx extention

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
 const plugin = require('tailwindcss/plugin')
 
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
     extend: {
       animation: {
