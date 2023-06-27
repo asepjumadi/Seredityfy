f762f1db0be21059e4d220f5c1867e74c0a3e200
Asep Jumadi <43208840+asepjumadi@users.noreply.github.com>
2023-06-28 01:33:51 +0700

    update Navbar.js
    add fixed and tranlation animation on navbar with useEffect and usestate

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import { Logo } from '../assets/import';
 import { useNavigate } from 'react-router-dom';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
@@ -6,13 +6,31 @@ import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 
 export const Navbar = () => {
     const navigate = useNavigate();
-    const [toggle, setToggle] = React.useState(false);
-    const handleClickRegister=()=>{
-            navigate('/signup');
-            navigate(0)
+    const [toggle, setToggle] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 0);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const handleClickRegister = () => {
+        navigate('/signup');
+        navigate(0)
     }
     return (
-        <nav className="bg-gray-800 bg-opacity-20 backdrop-blur-lg py-5">
+        <nav
+        className={`${isScrolled?'bg-slate-900':'bg-gray-800 bg-opacity-20 backdrop-blur-lg'} py-5 ${isScrolled?'fixed top-0':''} w-full z-50 transition duration-500 ease-in-out ${
+          isScrolled ? 'scrolled' : ''
+        }`}
+      >
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     <div className="flex-shrink-0">
@@ -33,7 +51,7 @@ export const Navbar = () => {
                             <a href="/update" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Update</a>
                             <a href="/price" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Price</a>
                             <div className="p-0.5 rounded bg-gradient-to-r from-fuchsia-600 via-fuchsia-500 to-fuchsia-500">
-                                <button className="px-6 py-2 text-white bg-gray-800 bg-opacity-90 backdrop-blur-xl text-xs" onClick={()=>handleClickRegister()}>Sign Up</button>
+                                <button className="px-6 py-2 text-white bg-gray-800 bg-opacity-90 backdrop-blur-xl text-xs" onClick={() => handleClickRegister()}>Sign Up</button>
                             </div>
                         </div>
                     </div>
@@ -52,7 +70,7 @@ export const Navbar = () => {
                         <a href="/update" className="text-gray-300 hover:bg-gray-700 hover:text-white py-4 rounded-md text-xl font-bold tracking-widest">Update</a>
                         <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white py-4 rounded-md text-xl font-bold tracking-widest">Price</a>
                         <div className="p-0.5 rounded w-[121.5px] bg-gradient-to-r from-fuchsia-600 via-fuchsia-500 to-fuchsia-500">
-                            <button className="px-6 py-2 text-white bg-gray-800 bg-opacity-90 backdrop-blur-xl text-xl" onClick={()=>handleClickRegister()}>Sign Up</button>
+                            <button className="px-6 py-2 text-white bg-gray-800 bg-opacity-90 backdrop-blur-xl text-xl" onClick={() => handleClickRegister()}>Sign Up</button>
                         </div>
                     </div>
                 </div>
