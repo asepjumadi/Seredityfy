3e7d95c1aecc36f4639045fb186505a2273a9304
Asep Jumadi <asepjumadi27@gmail.com>
2023-08-20 23:05:08 +0700

    [SFY-MR200823] update responsive navbar progress[50%]

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { Logo } from '../assets/import';
 import { useNavigate } from 'react-router-dom';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 
 
-export const Navbar = ({type=''}) => {
+export const Navbar = ({ type = '' }) => {
     const navigate = useNavigate();
     const [toggle, setToggle] = useState(false);
     const [isScrolled, setIsScrolled] = useState(false);
@@ -27,10 +27,9 @@ export const Navbar = ({type=''}) => {
     }
     return (
         <nav
-        className={`${isScrolled?'bg-slate-900':'bg-gray-800 bg-opacity-20 backdrop-blur-lg'} py-5 ${isScrolled?'fixed top-0':''} w-full z-50 transition duration-500 ease-in-out ${
-          isScrolled ? 'scrolled' : ''
-        }`}
-      >
+            className={`${isScrolled ? 'bg-slate-900' : 'bg-gray-800 bg-opacity-20 backdrop-blur-lg'} py-5 ${isScrolled ? 'fixed top-0' : ''} w-full z-50 transition duration-500 ease-in-out ${isScrolled ? 'scrolled' : ''
+                }`}
+        >
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16 mx-auto">
                     <div className="flex-shrink-0">
@@ -39,9 +38,9 @@ export const Navbar = ({type=''}) => {
                     <div className='block md:hidden'>
                         {
                             toggle ?
-                                <RiCloseLine color={`${isScrolled?'white':type=='update'?'grey':'black'}`} size={27} onClick={() => setToggle(false)} />
+                                <RiCloseLine color={`${isScrolled ? 'white' : type == 'update' ? 'grey' : 'black'}`} size={27} onClick={() => setToggle(false)} />
                                 :
-                                <RiMenu3Line color={`${isScrolled?'white':type=='update'?'grey':'black'}`} size={27} onClick={() => setToggle(true)} />
+                                <RiMenu3Line color={`${isScrolled ? 'white' : type == 'update' ? 'grey' : 'black'}`} size={27} onClick={() => setToggle(true)} />
                         }
                     </div>
                     <div className="hidden md:block  ">
@@ -59,22 +58,40 @@ export const Navbar = ({type=''}) => {
                 </div>
             </div>
             {
-                toggle && <div className="
-                z-50
-                transform after:translate-y-4 before::translate-y-6
-                transition duration-500 ease-in-out                
-                block md:hidden h-100 w-4/6 right-2 top-[80px] fixed">
-                    <div className="flex flex-col items-end lg:items-stretch py-10 px-10 lg:py-0 lg:mx-20 mt-[2px] lg:w-3/12 lg:h-[540px] lg:mb-[100px] bg-gray-800 bg-opacity-60 backdrop-blur-md shadow-sm shadow-white space-x-4">
+                toggle && <div className='pt-2 px-2 pb-96 absolute top-0 right-0 bg-black xs:z-40 w-screen h-fit items-center mx-auto transform after:translate-y-4 before::translate-y-6
+                transition duration-500 ease-in-out'>
+                    <div className='flex flex-row justify-between items-center gap-8 p-2'>
+                        <a className='p-2  rounded-md  cursor-pointer'>
+                            <img src={Logo} className='w-6 h-6 mr-4' alt={`${Logo} seredityfy `} />
+                        </a>
+                        <button
+                            className=" md:ml-auto text-white mr-2  bg-slate-900 p-2 rounded-full"
+                            onClick={() => setToggle(false)}
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-6 w-6"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M6 18L18 6M6 6l12 12"
+                                />
+                            </svg>
+                        </button>
+                    </div>
+                    <div className="flex flex-col items-start lg:items-stretch py-10 px-10 lg:py-0 lg:mx-20 mt-[2px] lg:w-3/12 lg:h-[540px] lg:mb-[100px] bg-opacity-60 backdrop-blur-md shadow-sm  ">
                         <a href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white py-4 rounded-md text-xl font-bold tracking-widest">Home</a>
                         <a href="/features" className="text-gray-300 hover:bg-gray-700 hover:text-white py-4 rounded-md text-xl font-bold tracking-widest">Features</a>
                         <a href="/update" className="text-gray-300 hover:bg-gray-700 hover:text-white py-4 rounded-md text-xl font-bold tracking-widest">Update</a>
                         <a href="/price" className="text-gray-300 hover:bg-gray-700 hover:text-white py-4 rounded-md text-xl font-bold tracking-widest">Price</a>
-                        <div className="p-0.5 rounded w-[121.5px] bg-gradient-to-r from-fuchsia-600 via-fuchsia-500 to-fuchsia-500">
-                            <button className="px-6 py-2 text-white bg-gray-800 bg-opacity-90 backdrop-blur-xl text-xl" onClick={() => handleClickRegister()}>Sign Up</button>
-                        </div>
+                        <button className="w-full py-6 mt-20 text-white border border-fuchsia-600 bg-gradient-to-br bg-gray-800 bg-opacity-90 backdrop-blur-xl text-xl" onClick={() => handleClickRegister()}>Sign Up</button>
                     </div>
                 </div>
-
             }
 
         </nav>
