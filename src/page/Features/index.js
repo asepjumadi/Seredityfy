60a4fa07f76c7cf653305505da00b2f4b197305e
Asep Jumadi <asepjumadi27@gmail.com>
2023-08-23 00:17:51 +0700

    [AN-230323-001] change background image with tailwinds image background

--- src/page/Features/index.js
+++ src/page/Features/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { ExpandPlatform } from '../../container/Feature/ExpandPlatform'
 import { Navbar } from '../../components/Navbar'
 import { Footer } from '../../components/Footer'
+import { MultiIntegration } from '../../container/Feature/MultiIntegration'
 
 export const Features = () => {
     return (
-        <div className=' bg-gradient-to-r from-blue-950 ml-0 to-zinc-900 h-fit'>
-            <Navbar type='nonupdate'/>
+        <div className=' bg-gradient-to-r ml-0 from-black via-emerald-950  to-black h-fit'>
+            <Navbar type='update'/>
+            <MultiIntegration/>
             <ExpandPlatform />
             <Footer/>
         </div>
