6bf5d269cde8c39bc5c8894f4ba70459ab7e739d
Asep Jumadi <asepjumadi27@gmail.com>
2023-08-23 15:35:17 +0700

    modified expandplatform.jsx add new card style

--- src/container/Feature/ExpandPlatform.jsx
+++ src/container/Feature/ExpandPlatform.jsx
@@ -3,31 +3,36 @@ import { dataPlatform } from '../../model/dummyFeatures'
 
 export const ExpandPlatform = () => {
     return (
-        <div className='flex flex-col backdrop-blur-xl bg-opacity-70 w-full h-screen  pt-10'>
+        <div className='flex flex-col bg-opacity-70 pt-10 xl:w-full xl:h-screen xl:pt-40'>
             <div className='flex flex-col items-center'>
-                <h1 className='text-white font-extrabold text-3xl tracking-wider'>Platform Support</h1>
+                <h1 className='text-white font-extrabold text-3xl tracking-wider xl:text-4xl'>Platform Support</h1>
             </div>
-            <div className='flex flex-col  items-center p-10 '>
-                <div className='items-center justify-center flex flex-row h-fit w-full '>
-
+            <div className='flex flex-col items-center p-5 xs:p-10 xl:p-16'>
+                <div className='flex flex-wrap justify-center -mx-2 gap-4'>
                     {
-                        dataPlatform.map((item, index) => {
-                            return (
-                                <div key={item.id} className='w-2/12 cursor-pointer shadow-[0px_4px_16px_rgba(255,255,216,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] mr-4'>
-                                    <div className='p-10 items-center flex-col flex bg-slate-900 rounded-lg'>
-                                        <img className='w-20 h-20 object-cover fill-slate-50' src={item.url} />
-                                        <h1 className='text-slate-200 mt-2'>{item.platform}</h1>
-                                        <p className='text-slate-200 mt-2'>{item.SupportVersion}</p>
-                                        <p className='text-slate-200 mt-2'>{item.SupportKarnelVersion}</p>
-                                    </div>
+                        dataPlatform.map((item, index) => (
+                            <div class="max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                <a href="#">
+                                    <img class="rounded-t-lg h-56 w-56" src={item.url} alt="" />
+                                </a>
+                                <div class="p-5">
+                                    <a href="#">
+                                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.platform}</h5>
+                                    </a>
+                                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{item.SupportKarnelVersion}-{item.SupportVersion}</p>
+                                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                        Read more
+                                        <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                        </svg>
+                                    </a>
                                 </div>
-
-                            )
-                        })
-
+                            </div>
+                        ))
                     }
                 </div>
             </div>
         </div>
     )
 }
+
